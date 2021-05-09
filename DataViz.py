55d10b13e9d7b403282c244b45a67effc7766165
jordyn <jordynmarlow@gmail.com>
2021-05-09 10:28:01 -0400

    Some improvements to DataViz.py (WIP, pushing EOD 5/8)

--- DataViz.py
+++ DataViz.py
@@ -6,35 +6,65 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 user_lat = 43.01272028760803
 user_lon = -83.71256602748012
 
-hospitals = pd.read_excel('sample_data.xlsx').set_index('name')
-
-
-class DataViz(QApplication):
+class DataViz(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.window_width, self.window_height = 1600, 1200
+        self.window_width, self.window_height = 1200, 800
         self.setMinimumSize(self.window_width, self.window_height)
-
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-
-        map = folium.Map(location=[user_lat, user_lon], tiles='Stamen Terrain', default_zoom_start=15)
-        for index, row in hospitals.iterrows():
-            folium.Marker(location=[row['lat'], row['lon']]).add_to(map)
+        self.hospitals = pd.read_excel('sample_data.xlsx').set_index('name')
+    
+        # -------------------------
+        # init map widget
+        # -------------------------
+        self.map = folium.Map(location=[user_lat, user_lon], tiles='Stamen Terrain', default_zoom_start=15)
+        for index, row in self.hospitals.iterrows():
+            folium.Marker(location=[row['lat'], row['lon']]).add_to(self.map)
 
         data = io.BytesIO()
-        map.save(data, close_file=False)
+        self.map.save(data, close_file=False)
 
-        webView = QWebEngineView()
-        webView.setHtml(data.getvalue().decode())
-        layout.addWidget(webView)
+        self.map_view = QWebEngineView(self)
+        self.map_view.resize(300, 300)
+        self.map_view.setHtml(data.getvalue().decode())
+
+        # -------------------------
+        # init distance widget
+        # -------------------------
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    #app.setStyleSheet('')
-    
-    data_viz = DataViz()
-    data_viz.show()
+        # -------------------------
+        # init capacity widget
+        # -------------------------
 
-    sys.exit(app.exec_())
+
+        # -------------------------
+        # init helipad widget
+        # -------------------------
+
+
+        # -------------------------
+        # init ventilators widget
+        # -------------------------
+
+
+        # -------------------------
+        # init tests widget
+        # -------------------------
+
+
+        # -------------------------
+        # init insurance widget
+        # -------------------------
+
+
+        # -------------------------
+        # init staff widget
+        # -------------------------
+
+
+
+app = QApplication(sys.argv)
+#app.setStyleSheet()
+window = DataViz()
+window.show()
+sys.exit(app.exec_())
