92ca19e1f5934a71113dde1e568afb533f368bfc
Jordyn Marlow <jordynmarlow@gmail.com>
2021-05-09 15:20:25 -0400

    Pushing midday changes 5/9

--- DataViz.py
+++ DataViz.py
@@ -1,4 +1,4 @@
-import matplotlib, folium, sys, io
+import matplotlib, folium, sys, io, requests, json
 import pandas as pd
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import QWebEngineView
@@ -23,14 +23,40 @@ class DataViz(QMainWindow):
         data = io.BytesIO()
         self.map.save(data, close_file=False)
 
-        self.map_view = QWebEngineView(self)
-        self.map_view.resize(300, 300)
-        self.map_view.setHtml(data.getvalue().decode())
+        self.map_widget = QWebEngineView(self)
+        self.map_widget.resize(300, 300)
+        self.map_widget.move(5, 5)
+        self.map_widget.setHtml(data.getvalue().decode())
 
         # -------------------------
         # init distance widget
         # -------------------------
+        lon = self.hospitals.loc['Hurley Medical Center']['lon']
+        lat = self.hospitals.loc['Hurley Medical Center']['lat']
+        r = requests.get(f"http://router.project-osrm.org/route/v1/driving/{user_lon},{user_lat};{lon},{lat}?overview=false""")
+        route = json.loads(r.content).get("routes")[0]
 
+
+        self.distance_widget = QGroupBox(self)
+        layout = QHBoxLayout()
+
+        distance_mi = str(round(route["legs"][0]["distance"] / 1609))
+        self.distance = QLabel(distance_mi, self)
+        layout.addWidget(self.distance)
+
+        distance_lbl = QLabel('miles\naway', self)
+        layout.addWidget(distance_lbl)
+
+        duration_min = str(round(route["legs"][0]["duration"] / 60))
+        self.duration = QLabel(duration_min, self)
+        layout.addWidget(self.duration)
+
+        duration_lbl = QLabel('minutes\naway', self)
+        layout.addWidget(duration_lbl)
+
+        self.distance_widget.setLayout(layout)
+        self.distance_widget.resize(300, 100)
+        self.distance_widget.move(5, 305)
 
         # -------------------------
         # init capacity widget
